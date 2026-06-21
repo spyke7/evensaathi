b1c6704b42b37dd87060be8e6884b9e0fa829e9c
spyke7 <dolaishreejan@gmail.com>
2026-06-21 20:59:38 +0530

    fixed client url

--- backend/main.py
+++ backend/main.py
@@ -53,7 +53,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://evensaathi.vercel.app/"],
+    allow_origins=["https://evensaathi.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
